45a2b8a50421ab07b18a9bbfa9d068a53f0301b1
Chris Alleaume <achris@vmware.com>
2020-06-03 14:38:40 -0500

    new samples

---
+++ ABX_Code.py
@@ -0,0 +1,24 @@
+def handler(context, inputs):
+    
+    outputs = {}
+    print("Executing ABX...")
+    
+    app_crit = inputs.customProperties.app_criticality
+    env = inputs.environment
+    outputs.customProperties = inputs.customerProperties
+    
+    # Make sure the object passed in is a disk (the objectType is defined on my Blueprint because I'm sharing this script for multiple subscriptions)
+    if inputs.customProperties.objectType == "disk":
+        outputs.customProperties.dataStore = "datastore1"
+
+    return outputs
+    
+  # Payload for customProperties looks like this
+  """ 
+  customProperties": {
+    "objectType": "disk",
+    "environment": "DEV",
+    "app_criticality": "1",
+    "provisioningType": "thin"
+  }
+  """
