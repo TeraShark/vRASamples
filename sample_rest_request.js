ecd6b1b2a199dedf3cbc94fd81e02993d29d487b
Chris Alleaume <achris@vmware.com>
2020-06-03 16:07:00 -0500

    Emebllished vRO JScript Code

--- sample_rest_request.js
+++ sample_rest_request.js
@@ -4,25 +4,25 @@ var host = RESTHostManager.createTransientHostFrom(_host);
 
 host.operationTimeout = 60;
 host.connectionTimeout = 30;
-//ignore cert errors:
+
+//Use this setting to ignore cert errors:
 host.hostVerification = false;
 
-//*** NOTE: ***
-//you must first import the certificate into vRO before you can establish a connection.
-//This is done by running the following Workflow:
-//name: Import a certificate from URL
-//inputs:   enter the https://fqdn of the host
-//          select the checkbox "Will you accept a certificate with warnings?"
-// ************
+/***********************      NOTE:      *************************
+If this is the first time you are connecting to this REST Host, 
+you must first import the certificate into vRO before you can establish a connection.
+This is done by running the following Workflow in vRO:
+name:       Import a certificate from URL
+inputs:     enter the https://fqdn of the REST host
+            select the checkbox "Will you accept a certificate with warnings?"
+******************************************************************/
 
 host.url = "https://cava8-vc-001350.sqa.local";
-
-//YWRtaW5pc3RyYXRvckB2c3BoZXJlLmxvY2FsOlZNd2FyZTEh
 
 //requestBody would be populated with payload if you are using a POST as the requestMethod
 var requestBody = null;
 var requestMethod = "GET";
-var request = host.createRequest(requestMethod, "rest/vcenter/vm", requestBody);
+var request = host.createRequest(requestMethod, "/rest/vcenter/vm", requestBody);
 
 // For Basic authentication: (I already have my basic token, so skipping in this sample)
 //var authParams = ['Shared Session', username, password];
@@ -32,7 +32,6 @@ var request = host.createRequest(requestMethod, "rest/vcenter/vm", requestBody);
 // Set Content Type
 request.contentType = "application/json";
 request.setHeader("Authorization", "Basic YWRtaW5pc3RyYXRvckB2c3BoZXJlLmxvY2FsOlZNd2FyZTEh");
-
 
 // Execute REST Request
 System.debug("REST Request: " + requestMethod + " " + request.fullUrl);
